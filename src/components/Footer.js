4b68426ffcca93ba45a44551698c25bc5d5bc177
David Okunade <davidokunade1@gmail.com>
2021-01-12 06:58:48 +0100

    added new social media handles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import { Link } from 'gatsby'
 
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
-//import vimeo from '../img/social/vimeo.svg'
+import facebook from '../img/social/facebook.svg'
+import linkedin from '../img/social/linkedin.svg'
 
 const Footer = class extends React.Component {
   render() {
@@ -72,10 +73,25 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
+                <a title="linkedin" href="https://www.linkedin.com/company/primace-agency">
+                  <img
+                    className="fas fa-lg"
+                    src={linkedin}
+                    alt="LinkedIn"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
                 <a title="instagram" href="https://instagram.com/primaceagency?igshid=bt2n926zd222">
                   <img
                     src={instagram}
                     alt="Instagram"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="facebook" href="https://www.facebook.com/primaceagency/">
+                  <img
+                    src={facebook}
+                    alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a><br />
