f21b4c79cb285f9bf9db439b92394813f18f4cbe
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2021-02-28 17:00:41 +0100

    test code ish

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -15,74 +15,16 @@ const Contact =  () => {
         <section style={{ background: '#fff', borderRadius: '50px 50px 0 0' }} className='section corner'>
           <div className='container'>
             <div className='content'>
-
-              <form
-                action='mailto:primaceagency@gmail.com'
-                method='post'
-                encType='text/plain'
-              >
-                <div className='field'>
-                  <label className='label' htmlFor='name'>
-                    Your name
-                  </label>
-                  <div className='control'>
-                    <input
-                      className='input'
-                      type='text'
-                      name='name'
-                      placeholder='Full Name'
-                      required
-                      id='name'
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label' htmlFor='email'>
-                    Email
-                  </label>
-                  <div className='control'>
-                    <input
-                      className='input'
-                      type='email'
-                      name='email'
-                      required
-                      id='email'
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label' htmlFor='subject'>
-                    Subject
-                  </label>
-                  <div className='control'>
-                    <input
-                      className='input'
-                      type='text'
-                      name='subject'
-                      id='subject'
-                      required
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <label className='label' htmlFor='message'>
-                    Message
-                  </label>
-                  <div className='control'>
-                    <textarea
-                      className='textarea'
-                      name='message'
-                      id='message'
-                      required
-                    />
-                  </div>
-                </div>
-                <div className='field'>
-                  <button className='button is-link' type='submit'>
-                    Send
-                  </button>
-                </div>
-              </form>
+    <form action="mailto:someone@example.com" method="post" enctype="text/plain">
+Name:<br>
+<input type="text" name="name"/><br>
+E-mail:<br>
+<input type="text" name="mail"/><br>
+Comment:<br>
+<input type="text" name="comment" size="50"/><br><br>
+<button type="submit">Submit </button>
+<input type="reset" value="Reset"/>
+</form>
 
             </div>
           </div>
